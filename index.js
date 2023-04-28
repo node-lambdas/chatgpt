f4ebad5cafa046db2095730622efe2a3f3c0c63f
Darlan Alves <me@darlanalv.es>
2023-04-28 22:31:11 +0000

    feat: add action for completions

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import { Configuration, OpenAIApi } from "openai";
 
-const defaultModel = 'gpt-3.5-turbo';
+const defaultModel = "gpt-3.5-turbo";
+const roles = ["assistant", "user", "system"];
+const validateMessage = (m) =>
+  typeof m === "object" && m && roles.includes(m.role) && m.content;
 
 export default {
   version: 2,
   actions: {
     ask: {
       default: true,
-      description: "Ask ChatGPT for a completion",
+      description: "Ask ChatGPT a question",
       credentials: ["authorization"],
       input: "text",
       output: "text",
@@ -24,6 +27,39 @@ export default {
             200,
             chat.data.choices.map((c) => c.message.content).join("")
           );
+        } catch (error) {
+          response.reject(String(error));
+        }
+      },
+    },
+    complete: {
+      description: "Ask ChatGPT for a completion",
+      credentials: ["authorization"],
+      input: "json",
+      output: "json",
+      async handler(request, response) {
+        const apiKey = request.headers.authorization;
+        const openai = new OpenAIApi(new Configuration({ apiKey }));
+        const input = request.body;
+
+        if (!Array.isArray(input) || !input.every(validateMessage)) {
+          response.writeHead(400);
+          response.end("Invalid chat stream");
+          return;
+        }
+
+        try {
+          const chat = await openai.createChatCompletion({
+            model: request.options.model || defaultModel,
+            messages: input,
+          });
+
+          const output = input.concat({
+            role: "assistant",
+            content: chat.data.choices.map((c) => c.message.content),
+          });
+
+          response.send(200, output);
         } catch (error) {
           response.reject(String(error));
         }
